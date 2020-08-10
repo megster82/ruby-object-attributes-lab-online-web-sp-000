e0adee17848387f2e4bcc0367716786eda7630e0
meaghan jennings <meaghanljennings@gmail.com>
2020-08-10 17:59:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,15 @@ class Person
     @name
   end
   
-  def name=(job)
-    @job = job
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+end 
